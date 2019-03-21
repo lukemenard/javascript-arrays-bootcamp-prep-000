f57380fb016bc2806538d482329c3e510478927e
Luke Menard <lukemenard@gmail.com>
2019-03-21 18:01:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,45 +1,45 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
   
-  function addElementToBeginningOfArray(array, element) {
-    [element, ...array]
-    return array
-  }
+function addElementToBeginningOfArray(array, element) {
+  [element, ...array]
+  return array
+}
   
-  function destructivelyAddElementToBeginningOfArray(array, element) {
-    array.unshift(element)
-    return array
-  }
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
   
-  function addElementToEndOfArray(array, element) {
-    [...array, element]
-    return array
-  }
+function addElementToEndOfArray(array, element) {
+  [...array, element]
+  return array
+}
   
-  function destructivelyAddElementToEndOfArray(array,element)
-  (array, element) {
-    array.push(element)
-    return array
-  }
+ function destructivelyAddElementToEndOfArray(array,element)
+(array, element) {
+  array.push(element)
+   return array
+}
   
-  function accessElementInArray(array, index) {
-    return array[index]
-  }
+function accessElementInArray(array, index) {
+  return array[index]
+}
   
-  function destructivelyRemoveElementFromBeginningOfArray(array){
-    return array.shift()
-  }
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
   
-  function removeElementFromBeginningOfArray(array){
-    array.slice(0)
-    return array
-  }
+function removeElementFromBeginningOfArray(array){
+  array.slice(0)
+  return array
+}
   
-  function destructivelyRemoveElementFromEndOfArray(array) {
-    array.pop()
-    return array
-  }
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
   
-  function removeElementFromEndOfArray(array) {
-    return array.slice(0, array.length - 1)
-  }
+function removeElementFromEndOfArray(array) {
+   return array.slice(0, array.length - 1)
+}
   
