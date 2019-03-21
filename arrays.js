7ec791edb38f4892eb5bac198a3024f2929731fe
Luke Menard <lukemenard@gmail.com>
2019-03-21 18:18:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,48 +3,4 @@ var chocolateBars = [
   'hundred grand',
   'kitkat',
   'skittles'
-]
-  
-function addElementToBeginningOfArray(array, element) {
-  [element, ...array]
-  return array
-}
-  
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  return array
-}
-  
-function addElementToEndOfArray(array, element) {
-  [...array, element]
-  return array
-}
-  
- function destructivelyAddElementToEndOfArray(array,element)
-(array, element) {
-  array.push(element)
-   return array
-}
-  
-function accessElementInArray(array, index) {
-  return array[index]
-}
-  
-function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.shift()
-}
-  
-function removeElementFromBeginningOfArray(array){
-  array.slice(0)
-  return array
-}
-  
-function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop()
-  return array
-}
-  
-function removeElementFromEndOfArray(array) {
-   return array.slice(0, array.length - 1)
-}
-  
+];
