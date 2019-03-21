432af9cd76bfed68fc665e751fc3800f1f1d33de
Luke Menard <lukemenard@gmail.com>
2019-03-21 18:27:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,8 +15,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  [...array, element]
-  return array
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array,element) {
